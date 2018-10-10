398cdddd75c8081b6b4aa6104ca95f9fab5ccfe0
Will Thompson <n9701192@qut.edu.au>
2018-10-10 17:41:07 +1000

    Added User class and manager

--- Cookout/User.cs
+++ Cookout/User.cs
@@ -1,10 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
 
 namespace Cookout
 {
-    class User
+    public class User
     {
+        int userID;
+        string firstName;
+        string lastName;
+        string email;
+        string password;
+        string imageURL;
+        string phoneNumber;
+
+        [JsonProperty(PropertyName = "userID")]
+        public int UserID
+        {
+            get { return userID; }
+            set { userID = value; }
+        }
+
+        [JsonProperty(PropertyName = "firstName")]
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        [JsonProperty(PropertyName = "lastName")]
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        [JsonProperty(PropertyName = "email")]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        [JsonProperty(PropertyName = "password")]
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        [JsonProperty(PropertyName = "imageURL")]
+        public string ImageURL
+        {
+            get { return imageURL; }
+            set { imageURL = value; }
+        }
+
+        [JsonProperty(PropertyName = "phoneNumber")]
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        [Version]
+        public string Version { get; set; }
     }
 }
---
+++ Cookout/UserManager.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+#if OFFLINE_SYNC_ENABLED
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+#endif
+
+namespace Cookout
+{
+    public partial class UserManager
+    {
+        static UserManager defaultInstance = new UserManager();
+        MobileServiceClient client;
+
+#if OFFLINE_SYNC_ENABLED
+        IMobileServiceSyncTable<user> userTable;
+#else
+        IMobileServiceTable<User> userTable;
+#endif
+
+        const string offlineDbPath = @"localstore.db";
+
+        private UserManager()
+        {
+            this.client = new MobileServiceClient(Constants.ApplicationURL);
+
+#if OFFLINE_SYNC_ENABLED
+            var store = new MobileServiceSQLiteStore(offlineDbPath);
+            store.DefineTable<User>();
+
+            //Initializes the SyncContext using the default IMobileServiceSyncHandler.
+            this.client.SyncContext.InitializeAsync(store);
+
+            this.userTable = client.GetSyncTable<user>();
+#else
+            this.userTable = client.GetTable<User>();
+#endif
+        }
+
+        public static UserManager DefaultManager
+        {
+            get
+            {
+                return defaultInstance;
+            }
+            private set
+            {
+                defaultInstance = value;
+            }
+        }
+
+        public MobileServiceClient CurrentClient
+        {
+            get { return client; }
+        }
+
+        public bool IsOfflineEnabled
+        {
+            get { return userTable is Microsoft.WindowsAzure.MobileServices.Sync.IMobileServiceSyncTable<User>; }
+        }
+
+        public async Task<ObservableCollection<User>> GetUsersAsync(bool syncItems = false)
+        {
+            try
+            {
+#if OFFLINE_SYNC_ENABLED
+                if (syncItems)
+                {
+                    await this.SyncAsync();
+                }
+#endif
+                //IEnumerable<TodoItem> items = await userTable
+                //    .Where(todoItem => !todoItem.Done)
+                //    .ToEnumerableAsync();
+
+                //return new ObservableCollection<User>(items);
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                Debug.WriteLine("Invalid sync operation: {0}", new[] { msioe.Message });
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Sync error: {0}", new[] { e.Message });
+            }
+            return null;
+        }
+
+        public async Task SaveTaskAsync(User user)
+        {
+            try
+            {
+                if (user.UserID == 0)
+                {
+                    await userTable.InsertAsync(user);
+                }
+                else
+                {
+                    await userTable.UpdateAsync(user);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Save error: {0}", new[] { e.Message });
+            }
+        }
+
+#if OFFLINE_SYNC_ENABLED
+        public async Task SyncAsync()
+        {
+            ReadOnlyCollection<MobileServiceTableOperationError> syncErrors = null;
+
+            try
+            {
+                await this.client.SyncContext.PushAsync();
+
+                await this.todoTable.PullAsync(
+                    //The first parameter is a query name that is used internally by the client SDK to implement incremental sync.
+                    //Use a different query name for each unique query in your program
+                    "allTodoItems",
+                    this.todoTable.CreateQuery());
+            }
+            catch (MobileServicePushFailedException exc)
+            {
+                if (exc.PushResult != null)
+                {
+                    syncErrors = exc.PushResult.Errors;
+                }
+            }
+
+            // Simple error/conflict handling. A real application would handle the various errors like network conditions,
+            // server conflicts and others via the IMobileServiceSyncHandler.
+            if (syncErrors != null)
+            {
+                foreach (var error in syncErrors)
+                {
+                    if (error.OperationKind == MobileServiceTableOperationKind.Update && error.Result != null)
+                    {
+                        //Update failed, reverting to server's copy.
+                        await error.CancelAndUpdateItemAsync(error.Result);
+                    }
+                    else
+                    {
+                        // Discard local change.
+                        await error.CancelAndDiscardItemAsync();
+                    }
+
+                    Debug.WriteLine(@"Error executing sync operation. Item: {0} ({1}). Operation discarded.", error.TableName, error.Item["id"]);
+                }
+            }
+        }
+#endif
+    }
+}
